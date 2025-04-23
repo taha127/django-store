b00fe0028f099e24cefa8738d455a7b4e9a8e7fd
taha127 <tahadehghani127@gmail.com>
2025-04-24 01:26:13 +0330

    update email backend settings for SMTP configuration

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,13 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 # allauth settings
 ACCOUNT_SESSION_REMEMBER = True
