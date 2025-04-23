fcc5a8a318675806a4a10454644b8e8478822713
taha127 <tahadehghani127@gmail.com>
2025-04-23 12:00:15 +0330

    update DEBUG setting to use DJANGO_DEBUG environment variable

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
 ALLOWED_HOSTS = []
 
