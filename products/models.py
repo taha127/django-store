72671d0ecb187ecf82945eda2aba792f025d37e4
taha127 <tahadehghani127@gmail.com>
2025-04-24 18:41:19 +0330

    add Comment model and migrations for product reviews

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -15,3 +17,20 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'very bad'),
+        ('2', 'bad'),
+        ('3', 'normal'),
+        ('4', 'good'),
+        ('5', 'perfect'),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
