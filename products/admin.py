c31c38d5dea912675228dba04d0254d86b090317
taha127 <tahadehghani127@gmail.com>
2025-04-25 17:08:33 +0330

    add CommentInline to ProductAdmin for inline comment management in admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 from products.models import Product, Comment
 
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+    fields = ('author', 'body', 'active', 'stars',)
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'active',)
-
+    inlines = [CommentInline, ]
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
