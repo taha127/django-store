f380a5c679d270951e7de4474665ba57915c100a
taha127 <tahadehghani127@gmail.com>
2025-04-25 16:33:08 +0330

    add get_absolute_url method to ProductReview model for product detail navigation

--- products/models.py
+++ products/models.py
@@ -34,3 +34,6 @@ class Comment(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'pk': self.product.id})
