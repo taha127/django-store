7a7b80f54789b4c76de906d7deac037fa7f45c51
taha127 <tahadehghani127@gmail.com>
2025-05-09 17:05:10 +0330

    update quantity choices in AddToCartProductForm to allow selection up to 30

--- cart/forms.py
+++ cart/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class AddToCartProductForm(forms.Form):
-    QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 30)]
+    QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 31)]
     quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
