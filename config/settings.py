8ffbf2ead38ad3795970e4e8cdf4ec33d3fdeab7
taha127 <tahadehghani127@gmail.com>
2025-05-16 18:16:35 +0330

    add media configuration to settings and update URL patterns for media serving

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,10 @@ LOCALE_PATHS = [
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+# Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
