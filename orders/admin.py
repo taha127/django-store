bb0e9d9dc9b86d7e23d6342ebcf626ff1b39196d
taha127 <tahadehghani127@gmail.com>
2025-07-25 13:13:29 +0330

    register Order and OrderItem models in admin with inline configuration

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order',
+              'product',
+              'quantity',
+              'price']
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'is_paid', 'datetime_created']
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
