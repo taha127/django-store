0e83ec6240334288b185b62e4936c3f97e5300a8
taha127 <tahadehghani127@gmail.com>
2025-07-25 14:05:19 +0330

    add order creation URL and view for order create template processing

--- config/urls.py
+++ config/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('rosetta/', include('rosetta.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def order_create_view(request):
+    return render(request, 'orders/order_create.html')
