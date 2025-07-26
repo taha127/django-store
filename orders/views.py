c9951f71956450c54cb1e89d9f9a13df68c0ec38
taha127 <tahadehghani127@gmail.com>
2025-07-26 12:22:52 +0330

    Add cart validation and order item creation in order view and save first_name & last_name in information user in addition Send messages for empty cart and success operation to place order

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import OrderForm
 from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+from .models import OrderItem
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 
 @login_required
 def order_create_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
+    if len(cart) == 0:
+        messages.warning(request, _('Your cart is empty. Please add products to your cart before placing an order.'))
+        return redirect('product_list')
     if request.method == 'POST':
         order_form = OrderForm(request.POST, )
         if order_form.is_valid():
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+            cart.clear()
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+            messages.success(request, _('Your order has been placed successfully!'))
     return render(request, 'orders/order_create.html', context={'form': order_form})
