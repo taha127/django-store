6be176ac73b1c75b44aa2fefeb35a3a27875fa57
taha127 <tahadehghani127@gmail.com>
2025-07-26 11:57:21 +0330

    Add order creation view with login requirement and form handling

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .forms import OrderForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def order_create_view(request):
-    return render(request, 'orders/order_create.html')
+    order_form = OrderForm()
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST, )
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+    return render(request, 'orders/order_create.html', context={'form': order_form})
