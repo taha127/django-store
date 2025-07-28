631f0fa6626f3be33b7779f5aa95247bbc853a98
taha127 <tahadehghani127@gmail.com>
2025-07-28 23:06:04 +0330

    Store order ID in session and redirect to payment process after successful order placement

--- orders/views.py
+++ orders/views.py
@@ -32,5 +32,6 @@ def order_create_view(request):
             request.user.first_name = order_obj.first_name
             request.user.last_name = order_obj.last_name
             request.user.save()
-            messages.success(request, _('Your order has been placed successfully!'))
+            request.session['order_id'] = order_obj.id
+            return redirect('payment:payment_process')
     return render(request, 'orders/order_create.html', context={'form': order_form})
