a0e4e363bf1eadab50c60415081bc98653292038
taha127 <tahadehghani127@gmail.com>
2025-07-28 23:06:34 +0330

    Add payment URL routing to support payment processing

--- config/urls.py
+++ config/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('orders.urls')),
+    path('payment/', include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
