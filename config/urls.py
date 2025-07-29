d0ce76ddf8d75a3c208cd69d9adf6d2fd9e3a1c6
taha127 <tahadehghani127@gmail.com>
2025-07-30 00:29:37 +0330

    Add debug toolbar URLs to urlpatterns for enhanced debugging in development

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,3 +31,8 @@ urlpatterns = [
     path('order/', include('orders.urls')),
     path('payment/', include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
