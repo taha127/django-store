6a9fa15c551040b649deb6fe719276d35640ccae
taha127 <tahadehghani127@gmail.com>
2025-07-29 18:03:00 +0330

    Add payment callback URL to handle Zarinpal payment verification

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import payment_process
+from .views import payment_process, payment_callback_view
 
 app_name = 'payment'
 urlpatterns = [
     path('process/', payment_process, name='payment_process'),
+    path('callback/', payment_callback_view, name='payment_callback'),
 ]
