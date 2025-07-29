a5b366c597db36bc77ad961473194ff5292a1028
taha127 <tahadehghani127@gmail.com>
2025-07-29 18:03:22 +0330

    Add Zarinpal reference ID and data fields to order model

--- orders/models.py
+++ orders/models.py
@@ -12,6 +12,8 @@ class Order(models.Model):
     is_paid = models.BooleanField(_('Is Paid?'), default=False)
     order_notes = models.CharField(_('Order Notes'), max_length=700, blank=True)
     zarinpal_authority = models.CharField(_('Authority'), max_length=255, blank=True)
+    zarinpal_ref_id = models.CharField(_('Reference ID'), max_length=150, blank=True)
+    zarinpal_data = models.TextField(_('Zarinpal Data'), blank=True)
     datetime_created = models.DateTimeField(_('DateTime Created'), auto_now_add=True)
     datetime_modified = models.DateTimeField(_('DateTime Modified'), auto_now=True)
 
